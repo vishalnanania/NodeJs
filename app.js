8cc7fd1546e68c09e174f07ee4694473c328775a
Nananiya <U1123352@na.ad.guycarp.com>
2018-01-16 14:49:52 -0500

    npm install nodemon -g : for auto rebuilding app when we change something, precess.argv for checking any additional arguments are paseed.

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 console.log('Starting app.js');
 
 const _ = require('lodash');
+// console.log(_.isString(true));
+// console.log(_.isString('Andrew'));
+// var filteredArray = _.uniq(['Andrew',1,1,1,'Andrew',2,4,3,2,3,4]);
+// console.log(filteredArray);
 
-console.log(_.isString(true));
-console.log(_.isString('Andrew'));
+var command = process.argv[2];
 
-var filteredArray = _.uniq(['Andrew',1,1,'Andrew',2,4,3,2,3,4]);
-
-console.log(filteredArray);
+if(command === 'add') {
+    console.log('Adding new note');
+}else if (command === 'list') {
+    console.log('listing all notes');
+}else if (command === 'read') {
+    console.log('Reading notes');
+}else if (command === 'remove') {
+    console.log('Removing note');
+}else {
+    console.log('command not recognized. e.g node app.js list,add,read,remove');
+} 
