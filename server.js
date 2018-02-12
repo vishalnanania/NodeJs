476e4dfcd3520f4dc062654e74c5236acae2d54e
Nananiya <U1123352@na.ad.guycarp.com>
2018-02-12 15:20:03 -0500

    express middleware

--- server.js
+++ server.js
@@ -1,32 +1,49 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
-app.set('view engine','hbs');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname, + '/public'));
 
-app.get('/', (req, res)=>{
-    res.render('home.hbs',{
-        pageTitle:'Home Page',
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}: ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err)=>{
+        if(err){
+            console.log(err);
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next)=>{
+//     res.render('maintenance.hbs');
+// });
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
         currentYear: new Date().getFullYear()
     });
 });
 
-app.get('/about', (req, res)=>{
-    res.render('about.hbs',{
-        pageTitle:'About Page',
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
         currentYear: new Date().getFullYear()
     });
 });
 
-app.get('/bad', (req, res)=>{
+app.get('/bad', (req, res) => {
     res.send({
         error: 'Bad request',
     })
 });
 
-app.listen(3001, ()=>{
+app.listen(3001, () => {
     console.log('Server is running at port 3001.');
 });
